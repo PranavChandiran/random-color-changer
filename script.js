ffdc444212eab14295c3c367b4a4c27202935f77
Pranav Chandiran <pranavchandiranj16@gmail.com>
2021-11-08 14:41:36 +0530

    added files

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("console was working !!!!");
+const btn = document.getElementById("btn");
+const txt = document.getElementById("txt");
+const body = document.getElementById("body");
+const copyBtn = document.getElementById("copyBtn");
+const alert01 = document.getElementById("alert1");
+
+btn.onclick = function () {
+  let r = Math.floor(Math.random() * 130);
+  let g = Math.floor(Math.random() * 100);
+  let b = Math.floor(Math.random() * 90);
+
+  body.style.background = `rgb(${r},${g},${b})`;
+  // alert01.style.display = "flex";
+  // (body.style.background = btn.style.background),
+  txt.innerHTML = `rgb(${r},${g},${b})`;
+
+  copyBtn.onclick = function () {
+    if (btn.onclick) {
+      navigator.clipboard.writeText(`rgb(${r},${g},${b})`);
+    }
+  };
+};
