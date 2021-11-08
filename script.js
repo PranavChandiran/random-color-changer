145468f20278436478f6924dc9e8a30631d08184
Pranav Chandiran <pranavchandiranj16@gmail.com>
2021-11-08 16:28:41 +0530

    some updates

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// console.log("console was working !!!!");
-// const btn = document.getElementById("btn");
-// const txt = document.getElementById("txt");
-// const body = document.getElementById("body");
+// // console.log("console was working !!!!");
+// // const btn = document.getElementById("btn");
+// // const txt = document.getElementById("txt");
+// // const body = document.getElementById("body");
 const copyBtn = document.getElementById("copyBtn");
-// const alert01 = document.getElementById("alert1");
+// // const alert01 = document.getElementById("alert1");
 
-// btn.onclick = function () {
-//   let r = Math.floor(Math.random() * 130);
-//   let g = Math.floor(Math.random() * 100);
-//   let b = Math.floor(Math.random() * 90);
+// // btn.onclick = function () {
+// //   let r = Math.floor(Math.random() * 130);
+// //   let g = Math.floor(Math.random() * 100);
+// //   let b = Math.floor(Math.random() * 90);
 
-//   body.style.background = `rgb(${r},${g},${b})`;
-//   // alert01.style.display = "flex";
-//   // (body.style.background = btn.style.background),
-//   txt.innerHTML = `rgb(${r},${g},${b})`;
+// //   body.style.background = `rgb(${r},${g},${b})`;
+// //   // alert01.style.display = "flex";
+// //   // (body.style.background = btn.style.background),
+// //   txt.innerHTML = `rgb(${r},${g},${b})`;
 
-//   copyBtn.onclick = function () {
-//     if (btn.onclick) {
-//       navigator.clipboard.writeText(`rgb(${r},${g},${b})`);
-//     }
-//   };
-// };
+// //   copyBtn.onclick = function () {
+// //     if (btn.onclick) {
+// //       navigator.clipboard.writeText(`rgb(${r},${g},${b})`);
+// //     }
+// //   };
+// // };
 
 console.log(copyBtn);
